b3b27687baf7ed8f05889dad951c23cc27d27b22
YamaTaro38 <yamadotaro38@gmail.com>
2026-06-14 12:24:07 +0700

    feat: add ApiKeySeeder via env variable, update DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             AdminDashboardSeeder::class,
             AdminUserSeeder::class,
+            ApiKeySeeder::class,
             ShopeeCategoryCodesSeeder::class
         ]);
     }
